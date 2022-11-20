0d92376442fa5eb53681fa6c740d356a1063b623
Adrian Caballero <92646371+Schlaufuchs17@users.noreply.github.com>
2022-11-20 19:03:19 +0100

    Madre mia, estaba mal escrito el Tomb Raider 4 ><
    Corregido el nombre de Tomb Raider 4 y modificados comentarios

--- SpringBootRest/src/main/java/serviciorest/modelo/persistencia/DaoVideojuego.java
+++ SpringBootRest/src/main/java/serviciorest/modelo/persistencia/DaoVideojuego.java
@@ -14,21 +14,13 @@ public class DaoVideojuego {
 	public List<Videojuego> listaJuegos;
 	public int id;
 
-	/**
-	 * Cuando se cree el objeto dentro del contexto de Spring, se ejecutara su
-	 * constructor, que creara los videojuegos y los metera en una lista para que
-	 * puedan ser consumidas por nuestros clientes
-	 * /* */
-
-	 //Se crea la lista de videojuegos
-
 	public DaoVideojuego() {
-		System.out.println("DaoVideojuegos -> Creando la lista de videojugos!");
+		System.out.println("Creando la lista de videojeugos");
 		listaJuegos = new ArrayList<Videojuego>();
 		Videojuego p1 = new Videojuego(id++, "Bloodborne", "From Software", 91);// ID: 0
 		Videojuego p2 = new Videojuego(id++, "Red Dead Redemption 2", "Rockstar", 95);// ID: 1
 		Videojuego p3 = new Videojuego(id++, "Warcraft 3 The Frozen Throne", "Blizzard Entertainment", 85);// ID: 2
-		Videojuego p4 = new Videojuego(id++, "Tomb Raider The Lasta Revelation", "Eidos", 82);// ID:3
+		Videojuego p4 = new Videojuego(id++, "Tomb Raider The Last Revelation", "Eidos", 82);// ID:3
 		Videojuego p5 = new Videojuego(id++, "Metal Gear Solid", "Konami", 87);// ID:4
 		listaJuegos.add(p1);
 		listaJuegos.add(p2);
@@ -37,13 +29,6 @@ public class DaoVideojuego {
 		listaJuegos.add(p5);
 	}
 
-	/**
-	 * Devuelve un videojuego a partir de su posicion del array
-	 * 
-	 * @param posicion la posicion del arrya que buscamos
-	 * @return el videojuego que ocupe en la posicion del array, null en caso de que no
-	 *         exista o se haya ido fuera de rango del array
-	 */
 	public Videojuego get(int posicion) {
 		try {
 			return listaJuegos.get(posicion);
@@ -53,20 +38,11 @@ public class DaoVideojuego {
 		}
 	}
 
-	/**
-	 * Metodo que devuelve el videojuego del array
-	 * 
-	 * @return una lista con todas los videojuegos del array
-	 */
 	public List<Videojuego> list() {
 		return listaJuegos;
 	}
 
-	/**
-	 * Metodo que introduce un videojuego al final de la lista
-	 * 
-	 * @param p el videojuego que queremos introducir
-	 */
+	//AÑADIR VIDEOJUEGO
 	public String add(Videojuego p) {
 
 		for (Videojuego x : listaJuegos) {
@@ -80,13 +56,7 @@ public class DaoVideojuego {
 		return "añadido";
 	}
 
-	/**
-	 * Borramos un videojuego de una posicion del array
-	 * 
-	 * @param posicion la posicion a borrar
-	 * @return devolvemos el videojuego que hemos quitado del array, o null en caso de
-	 *         que no exista.
-	 */
+	//BORRAR VIDEOJUEGO
 	public Videojuego delete(int posicion) {
 		try {
 			return listaJuegos.remove(posicion);
@@ -96,13 +66,7 @@ public class DaoVideojuego {
 		}
 	}
 
-	/**
-	 * Metodo que modifica un videojuego de una posicion del array
-	 * 
-	 * @param p contiene todos los datos que queremos modificar, pero p.getId()
-	 *          contiene la posicion del array que queremos eliminar
-	 * @return el videojuego modificado en caso de que exista, null en caso contrario
-	 */
+	//MODIFICAR VIDEOJUEGO
 	public Videojuego update(Videojuego p) {
 		try {
 			Videojuego pAux = listaJuegos.get(p.getId());
@@ -117,15 +81,7 @@ public class DaoVideojuego {
 		}
 	}
 
-	/**
-	 * Metodo que devuelve todas los videojuegos por nombre. Como puede haber varios
-	 * videojuegos con el mismo nombre, tengo que devolver una lista con todas
-	 * los encontrados
-	 * 
-	 * @param nombre representa el nombre por el que vamos a hacer la busqueda
-	 * @return una lista con los videojuegos que coincidan en el nombre. La lista
-	 *         estará vacia en caso de que no hay coincidencias
-	 */
+	//LSITAR VIDEOJUEGOS
 	public List<Videojuego> listByNombre(String nombre) {
 		List<Videojuego> listajuegoAux = new ArrayList<Videojuego>();
 		for (Videojuego p : listaJuegos) {
